182287a9edb9b7ed3af76a16c69eb33a046c135d
Atoll <yann631@gmail.com>
2025-03-29 20:25:23 +0100

    com

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,16 +1,16 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { CommandInteraction } from 'discord.js';
+import { CacheType, CommandInteraction, SlashCommandBuilder } from 'discord.js';
 
-export const data = new SlashCommandBuilder()
+export default {
+  data: new SlashCommandBuilder()
     .setName('roll')
     .setDescription('Génère un nombre aléatoire entre 0 et le nombre donné.')
     .addIntegerOption(option => 
         option.setName('max')
             .setDescription('Le nombre maximum du tirage')
             .setRequired(true)
-    );
+    ),
 
-export async function execute(interaction: CommandInteraction) {
+  async execute(interaction: CommandInteraction<CacheType>) {
     const max = interaction.options.getInteger('max', true);
     if (max < 0) {
         await interaction.reply('Veuillez entrer un nombre positif.');
@@ -19,4 +19,5 @@ export async function execute(interaction: CommandInteraction) {
     
     const randomNumber = Math.floor(Math.random() * (max + 1));
     await interaction.reply(`🎲 Résultat du roll : **${randomNumber}** (entre 0 et ${max})`);
-}
+  },
+};
