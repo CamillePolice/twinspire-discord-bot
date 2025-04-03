c17b436241f3ab11330d901b4d7d278a329868cf
Atoll <yann631@gmail.com>
2025-04-03 19:51:22 +0200

    update event file

--- src/events/event.ts
+++ src/events/event.ts
@@ -12,11 +12,25 @@ export default {
       'Bière', 'Bieres', 'Biere', 'Bières',
       'BIÈRE', 'BIERES', 'BIERE', 'BIÈRES'
     ];
+    
+    const twinspireVariations = [
+      'twinspire'
+    ];
+
+    const images = ['./images/supporter_1.png', './images/supporter_2.png'];
 
     if (biereVariations.some(variant => message.content.includes(variant))) {
       if (message.channel instanceof TextChannel) {
         console.log("Message détecté, réponse envoyée !"); // DEBUG
         await message.channel.send("Kuroooo ! Quelqu'un t'appelle ! Viens vite !");
+      }
+    }
+
+    if (twinspireVariations.some(variant => message.content.includes(variant))) {
+      if (message.channel instanceof TextChannel) {
+        const randomImage = images[Math.floor(Math.random() * images.length)];
+        console.log("Message détecté, envoi de l'image !"); // DEBUG
+        await message.channel.send({ files: [randomImage] });
       }
     }
   },
