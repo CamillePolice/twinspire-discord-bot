775c0877e81e4387d8f301681308c021089f524b
Camille Police <camillepolice@icloud.com>
2025-04-08 21:56:03 +0200

    fix: wrong import

--- bot/commands/builders/roll.builders.ts
+++ bot/commands/builders/roll.builders.ts
@@ -1,25 +1,29 @@
-import { CacheType, CommandInteraction, SlashCommandBuilder, CommandInteractionOptionResolver } from 'discord.js';
+import {
+  CacheType,
+  CommandInteraction,
+  SlashCommandBuilder,
+  CommandInteractionOptionResolver,
+} from 'discord.js';
 import { randomInt } from 'crypto';
+import { CommandBuilder } from '../types';
 
 export const buildRollCommand: CommandBuilder<CommandInteraction> = {
   data: new SlashCommandBuilder()
-  .setName('roll')
-  .setDescription('Génère un nombre aléatoire entre 0 et le nombre donné.')
-  .addIntegerOption(option => 
-      option.setName('max')
-          .setDescription('Le nombre maximum du tirage')
-          .setRequired(true)
-  ),
+    .setName('roll')
+    .setDescription('Génère un nombre aléatoire entre 0 et le nombre donné.')
+    .addIntegerOption(option =>
+      option.setName('max').setDescription('Le nombre maximum du tirage').setRequired(true),
+    ) as SlashCommandBuilder,
 
   execute: async (interaction: CommandInteraction<CacheType>) => {
     const options = interaction.options as CommandInteractionOptionResolver;
     const max = options.getInteger('max', true);
-    
+
     if (max < 0) {
-        await interaction.reply('Veuillez entrer un nombre positif.');
-        return;
+      await interaction.reply('Veuillez entrer un nombre positif.');
+      return;
     }
-    
+
     const randomNumber = randomInt(0, max + 1); // Utilisation de randomInt de crypto
 
     await interaction.reply(`🎲 Résultat du roll : **${randomNumber}** (entre 0 et ${max})`);
